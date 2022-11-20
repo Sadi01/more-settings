8cb46899175cc69a22006e2aa85fd341b490b487
Sadegh Shafii <Sadshafiei.01@gmail.com>
2022-11-20 20:29:41 +0330

    Create Bootstrap.php

---
+++ src/Bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace sadi01\postecommerce;
+
+use WebApplication;
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+use yii\i18n\PhpMessageSource;
+
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        if (!isset($app->get('i18n')->translations['more-settings*'])) {
+            $app->get('i18n')->translations['more-settings*'] = [
+                'class' => PhpMessageSource::className(),
+                'basePath' => __DIR__ . '/messages',
+                'sourceLanguage' => 'en-US',
+                'fileMap' => [
+                    'more-settings' => 'main.php',
+                ],
+            ];
+        }
+    }
+}
